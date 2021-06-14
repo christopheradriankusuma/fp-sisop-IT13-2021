0c5e5d18104a8a8861d2e0c109dc0f5c5ba9235a
christopheradriankusuma <christopheradrian2903@gmail.com>
2021-06-14 19:15:36 +0700

    edit database

--- database/yoursql.c
+++ database/yoursql.c
@@ -29,7 +29,7 @@ char database[1024] = "";
 char username[1024] = "";
 char password[1024] = "";
 char buffer[1024] = "";
-char *server_path = "/home/kali/Desktop/Sisop/FP/server";
+char *server_path = "/home/kali/Desktop/Sisop/FP/fp-sisop-IT13-2021";
 char query_result[1024] = "";
 char error[1024] = "";
 
@@ -1438,6 +1438,12 @@ void dump(char *db) {
 
 
 int main(int argc, char const *argv[]) {
+    system("mkdir -p rootdb");
+    system("touch rootdb/users");
+    system("du rootdb/users | if [ `cut -f1` -eq 0 ]; then echo 'user|string\tpassword|string\nroot\troot' > rootdb/users; fi");
+    system("touch rootdb/db_permission");
+    system("du rootdb/db_permission | if [ `cut -f1` -eq 0 ]; then echo 'username|string\tdatabase|string\nroot\trootdb' > rootdb/db_permission; fi");
+
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
